96d26e71ce1f59bd28f8e5ad1faa329990f71602
snajim2 <sabienn1129@gmail.com>
2026-07-29 13:35:29 -0400

    2. Simple Messaging Added

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import argparse
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+if api_key is None:
+    raise RuntimeError("API Key not aquired.")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
 def main():
-    print("Hello from ai-agent!")
+    messages = [
+    {
+        "role": "user",
+        "content": "",
+    },
+]
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+    messages[0]['content'] = args.user_prompt
+
+    response = client.chat.completions.create(model="openrouter/free", messages=messages)
+
+    if args.verbose:
+        print(f"User prompt: {messages[0]['content']}")
+        print(f"Prompt tokens: {response.usage.prompt_tokens}")
+        print(f"Response tokens: {response.usage.completion_tokens}")
+    print("Response: ")
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
